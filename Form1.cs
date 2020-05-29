5a3eca22cd9d8d1244eaf6d703f47c38c4444ded
Crekan <Crekan@github.com>
2020-05-29 22:34:24 +0300

    MD5-Hash
    hashing

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MD5_Hash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetHash(string input)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            return Convert.ToBase64String(hash);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = GetHash(textBox1.Text);
+        }
+    }
+}
